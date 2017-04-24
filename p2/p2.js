177cae3acbb9d83169991f68b1cac37cd35b563c
lisnic <lisnic@1228fd1f-c061-4b60-9e78-fcac84c2d0e3>
2017-04-24 16:57:20 +0000

    done univariate maps

--- p2/p2.js
+++ p2/p2.js
@@ -41,9 +41,43 @@ const cmlSize = 210; // width and height of picture of colormap
   functions you want to help implement those three functions. All the comments
   (including this) are tips/hints that you can erase if you want. */
 
+//declare range global vars
+var ARrange, EMrange, UNrange, OBrange, IMrange, 
+	VUrange, WUrange, VBrange, WBrange, 
+	ESwrange, ESmrange, ERwrange, ERmrange;
+
+
 function rowFinish(d) {
     /* compute here the information about each state that will be needed for
        visualization later (e.g. unemployment rate)) */
+
+    // loading in the variables
+    	d.Area = +d.Area;
+    	d.Population = +d.Population;
+   	// calculating unemployment rate
+    	d.UnempRate = 100-100*(+d.Employed/+d.LaborForce);
+    // calculating employment rate
+    	d.EmpRate = 100*(+d.Employed/+d.Population);
+    	d.WomenEarning = +d.WomenEarning;
+    	d.MenEarning = +d.MenEarning;
+    	d.Obesity = +d.Obesity;
+    	d.InfantMortality = +d.InfantMortality;
+    // calculating political leanings in the elections
+    	d.PL1 = (+d.Obama)/(1+(+d.Romney)+(+d.Obama));
+    	d.PL2 = (+d.Clinton)/(1+(+d.Trump)+(+d.Clinton));
+    // calculating total voters, initiating variables for further use
+    	d.VT = Math.max(((+d.Obama)+(+d.Romney)), ((+d.Clinton)+(+d.Trump)));
+    	d.VF = 0;
+    	d.VA = 0;
+    	d.WF = 0;
+    	d.WA = 0;
+    	d.esw = 0;
+    	d.esm = 0;
+    	d.erw = 0;
+    	d.erm = 0;
+
+   		console.log(d);
+
     return d;  // keep this line, or else data becomes empty
 }
 
@@ -58,6 +92,190 @@ function dataFinish(data) {
        display: e.g. how to retrieve that variable from each element of the
        data array, the min-to-max extent (as from d3.extent()) of that
        variable, and which colormap to use. */
+
+
+    // calculating parameters for voter maps
+       var VTmax = d3.max(data, function(d) { return +d.VT; });
+       data.map(function(d) { d.VF = ((+d.Obama)+(+d.Romney))/VTmax});
+       data.map(function(d) { d.VA = 1 - Math.pow((1-d.VF),3) });
+       data.map(function(d) { d.WF = ((+d.Clinton)+(+d.Trump))/VTmax});
+       data.map(function(d) { d.WA = 1 - Math.pow((1-d.WF),3) });
+    // calculating params for earnings
+       var Emax = d3.max(data, function(d) { return Math.max(d.WomenEarning,d.MenEarning); });
+       var EWmax = d3.max(data, function(d) { return +d.WomenEarning});
+       var EMmax = d3.max(data, function(d) { return +d.MenEarning});
+       var EWmin = d3.min(data, function(d) { return +d.WomenEarning});
+       var EMmin = d3.min(data, function(d) { return +d.MenEarning});
+       data.map(function(d) { d.esw = d.WomenEarning/Emax});
+       data.map(function(d) { d.esm = d.MenEarning/Emax});
+       data.map(function(d) { d.erw = (d.WomenEarning-EWmin)/(EWmax-EWmin)});
+       data.map(function(d) { d.erm = (d.MenEarning-EMmin)/(EMmax-EMmin)});
+    // calculating ranges
+       ARrange = d3.extent(data, function(d){ return +d.Area});
+       EMrange = d3.extent(data, function(d){ return +d.EmpRate});
+       UNrange = d3.extent(data, function(d){ return +d.UnempRate});
+       OBrange = d3.extent(data, function(d){ return +d.Obesity});
+       IMrange = d3.extent(data, function(d){ return +d.InfantMortality});
+       VUrange = d3.extent(data, function(d){ return +d.PL1});
+       WUrange = d3.extent(data, function(d){ return +d.PL2});
+       VBrange = d3.extent(data, function(d){ return +d.VA});
+       WBrange = d3.extent(data, function(d){ return +d.WA});
+       ESwrange = d3.extent(data, function(d){ return +d.esw});
+       ESmrange = d3.extent(data, function(d){ return +d.esm});
+       ERwrange = d3.extent(data, function(d){ return +d.erw});
+       ERmrange = d3.extent(data, function(d){ return +d.erm});
+
+}
+
+// function that returns appropriate colorscale given type of map
+function getColorScale(wat) {
+
+	var cs;
+
+	switch(wat) {
+	// area : simple black-white log scale, works pretty well
+	case "AR":
+		cs = d3.scaleLog()
+				.domain(ARrange)
+				.range([d3.rgb(0,0,0), d3.rgb(255,255,255)])
+				.base(2);
+		return cs;
+		//return cs(d.Area);
+	// unemployment rate: rgb scale with 4 steps
+	case "UN":
+		cs = d3.scaleLinear()
+				.domain([UNrange[0], 
+					(1/3)*(UNrange[1]-UNrange[0])+UNrange[0],
+					(2/3)*(UNrange[1]-UNrange[0])+UNrange[0], 
+					UNrange[1]])
+				.range([d3.rgb(0,0,0), 
+					d3.rgb(230,0,0), 
+					d3.rgb(255,230,0), 
+					d3.rgb(255,255,255)]);
+		return cs;
+		//return cs(d.UnempRate);
+	// employment rate: inverse of above
+	case "EM":
+		cs = d3.scaleLinear()
+				.domain([EMrange[0], 
+					(1/3)*(EMrange[1]-EMrange[0])+EMrange[0],
+					(2/3)*(EMrange[1]-EMrange[0])+EMrange[0],  
+					EMrange[1]])
+				//.interpolate(d3.interpolateRgb)
+				.range([d3.rgb(255,255,255), 
+					d3.rgb(255,230,0), 
+					d3.rgb(230,0,0), 
+					d3.rgb(0,0,0)]);
+		return cs;
+		//return cs(d.EmpRate);
+	// infant mortality: transform domain to [0,1], hcl scales
+	case "IM":
+		var X = d3.scaleLinear().domain(IMrange).range([0,1]);
+		var H = d3.scaleLinear().domain([0,1])
+					.range([330,0]);
+		function C(x) { return 23*Math.pow(Math.sin(Math.PI*x),2)};
+		function L(x) { return 10+90*x};
+		cs = function(a) { return d3.hcl(H(X(a)), C(X(a)), L(X(a)))};
+		return cs;	
+	// obesity: hsl scale with 3 steps, 10 ticks
+	case "OB":
+		var mid = (1/2)*(OBrange[1]-OBrange[0]) + OBrange[0];
+		var hslcolors = ["hsl(150,100%,50%)",
+						"hsl(150,0%,50%)",
+						"hsl(330,100%,50%)"];
+		cs = d3.scaleLinear()
+				.domain([OBrange[0],mid,OBrange[1]])
+				.interpolate(d3.interpolateHsl)
+				.range(hslcolors);
+		cs.ticks();
+		return cs;
+		//return cs(d.Obesity);	
+	// earnings: simple lab mapping
+	case "ES":
+	cs = function (a,b) { return d3.lab((30+45*(a+b)),
+						0,
+						230*(a-b))};
+		return cs;
+	case "ER":
+		cs = function (a,b) { return d3.lab((30+45*(a+b)),
+						0,
+						230*(a-b))};
+		return cs;
+	// Obama/Romney uni :
+	case "VU":
+		var c1 = d3.rgb(210,0,0);
+		var c2 = d3.rgb(0,0,210);
+		var H1 = d3.hcl(c1).h;
+		var H2 = d3.hcl(c2).h;
+		console.log(H1,H2);
+		function Hu(x) { if (x<0.5)
+						 {return H1}
+						else 
+						 {return H2}};
+		var Cmin = d3.hcl(c1).c;
+		var Cmax = d3.hcl(c2).c;
+		function Cu(x) { if (x<0.5) 
+						 {return Cmin*(1-Math.pow((1-Math.abs(x-0.5)/0.5),4))}
+						else 
+						 {return Cmax*(1-Math.pow((1-Math.abs(x-0.5)/0.5),4))}};
+		var Lu = d3.scaleLinear()
+					.domain([0,0.5,1])
+					.range([d3.hcl(c1).l,
+							d3.hcl("darkgray").l,
+							d3.hcl(c2).l]);
+		cs = function(xu) {return d3.hcl(Hu(xu),Cu(xu),Lu(xu))};
+		return cs;
+	}
+	//return d3.rgb(12, 67, 199);
+}
+
+//apply coloscale for appropriate data
+function colorme(wat,d){
+	var cs = getColorScale(wat);
+	switch(wat){
+		case "AR": return cs(d.Area);
+		case "UN": return cs(d.UnempRate);
+		case "EM": return cs(d.EmpRate);
+		case "OB": return cs(d.Obesity);
+		case "IM": return cs(d.InfantMortality);
+		case "ES": return cs(d.esm, d.esw);
+		case "ER": return cs(d.erm, d.erw);
+		case "VU": return cs(d.PL1);
+	}
+}
+
+//return range given type of map
+function getRange(wat){
+	var cs = getColorScale(wat);
+	switch(wat){
+		case "AR": return ARrange;
+		case "UN": return UNrange;
+		case "EM": return EMrange;
+		case "OB": return OBrange;
+		case "IM": return IMrange;
+		case "ES": return ESmrange;
+		case "ER": return ERmrange;
+		case "VU": return [0,1]
+	}
+}
+
+function canvasme(wat,d){
+// so far only univariate
+	var watrange = getRange(wat);
+	var canvasscl = d3.scaleLinear()
+						.range(watrange)
+						.domain([0,p2.cmlSize]);
+	var cs = getColorScale(wat);
+
+	for (var j=0, k=0; j < p2.cmlSize; ++j) {
+      for (var i=0; i < p2.cmlSize; ++i) {
+      	var cc = cs(canvasscl(i));
+          p2.cmlImage.data[k++] = d3.rgb(cc).r; // red
+          p2.cmlImage.data[k++] = d3.rgb(cc).g; // green
+          p2.cmlImage.data[k++] = d3.rgb(cc).b; // blue
+          p2.cmlImage.data[k++] = 255; // opacity; keep at 255
+      }
+  }
 }
 
 function choiceSet(wat) {
@@ -74,6 +292,8 @@ function choiceSet(wat) {
        d3.select("#mapUS").selectAll("path").data(p2.usData) ... and set the
        color with .style("fill", function(d){ return ... color ...; })*/
 
+    d3.select("#mapUS").selectAll("path").data(p2.usData)
+    	.style("fill", function(d){ return colorme(wat,d)});
     /* 2) reset pixels of cmlImage.data, by traversing it via (see index.html):
        for (var j=0, k=0; j < p2.cmlSize; j++) {
            for (var i=0; i < p2.cmlSize; i++) {
@@ -89,6 +309,9 @@ function choiceSet(wat) {
        p2.cmlContext.putImageData(p2.cmlImage, 0, 0);
        Transitions on canvases are more work, so it is okay for this colormap
        image to change suddenly (w/out transition of duration p2.transDur) */
+
+       canvasme(wat,p2.usData);
+  		p2.cmlContext.putImageData(p2.cmlImage, 0, 0);
 
     /* 3) Update the labels at the corners of the colormap with
        d3.select("#xminlabel").html("<text>" + x0 + "</text>"); where x0 is
